e3b5aab98ef9bfed580b57a58fdc0695df3d2304
Anthony Ybarra <anthony_jy72@yahoo.com>
2019-12-18 15:52:49 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,17 @@
 def square_array(array)
-  # your code here
+  counter = 0
+
+numbers = [1,2,3]
+while numbers[counter] do
+  puts numbers[counter]
+  counter += 1
+  square_array(numbers)
+end
+
+new_numbers = [9,10,16,25]
+  counter = 0
+  square_array(new_numbers)
+  while new_numbers(array) do
+    puts new_numbers[counter]
+    counter += 1
 end
