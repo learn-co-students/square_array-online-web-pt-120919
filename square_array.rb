7dd432bccaf40b3b4004e8caf524c7e68d8aba60
Nguyen Ha <nynha0526@gmail.com>
2019-12-21 23:38:28 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,5 @@
-def square_array[2**2, 3**2, 4**2]
-  square_array.each do |square|
-    puts "#{square_array}"
+def square_array(array)
+  squared = []
+  array.each { |element| squared << element ** 2 }
+  squared
 end
