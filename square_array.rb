d20c3f428cab9e43480cbfc4b2d1e38d6067c15a
christopher tate <chris29tate2@hotmail.co.uk>
2019-12-22 13:49:11 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,25 +1,8 @@
 def square_array(array)
-  new_array = [1, 2 ,3 ]
-counter = 0
-
-while array < array.length do
-new_array << array[counter] **2
-  counter += 1
-  return new_array
+  results = []
   
-end
-
-
-def square_array(ary)
-  a = Array.new(ary.length)
-  ary.each_index { |i| a[2] = ary[4] * ary[7] }
-  a
-end
-
-
-class Array
-def square!
-self.map { |num 1, 2, 3| num ** 2}
-self
-end
+  array.each do |number|
+    results << (number*number)
+end  
+results
 end
